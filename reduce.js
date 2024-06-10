5cbc2856efb5e811e6bbfeb5ecc97ba2b7a8f222
Andrew O'Dwyer <andrewodwyer@yahoo.co.uk>
2024-06-10 08:12:57 +0000

    reduce method 1

--- reduce.js
+++ reduce.js
@@ -7,6 +7,26 @@
 
 // Summing an array of numbers:
 
+const nums = [0, 1, 2, 3, 4];
+let sum = nums.reduce((acc, curr) => acc + curr, 10); 
+//The comma is separating the 1st and 2nd parameters to reduce(): the callback function and the initial value
+console.log(sum);
+
+
+let sum3 = nums.reduce((acc, curr) => acc + curr); 
+//The comma is separating the 1st and 2nd parameters to reduce(): the callback function and the initial value
+console.log("curr or current value starts at the second value if it's not told what number to start on. in this case, current value is 1", sum3);
+console.log(sum3);
+
+let sum1 = nums.reduce((acc, curr) => {
+  console.log(
+    "Accumulator", acc,
+    "Current Value:", curr,
+    "Total:", acc + curr
+  );
+  return acc + curr;
+}, 10);
+console.log(sum1);
 
 const teamMembers = [
   {
