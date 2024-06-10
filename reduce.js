561be625841ac0a46bf1a0c32e8e5821e853b04c
Andrew O'Dwyer <andrewodwyer@yahoo.co.uk>
2024-06-10 08:40:45 +0000

    reduce 2

--- reduce.js
+++ reduce.js
@@ -8,7 +8,7 @@
 // Summing an array of numbers:
 
 const nums = [0, 1, 2, 3, 4];
-let sum = nums.reduce((acc, curr) => acc + curr, 10); 
+let sum = nums.reduce((acc, curr) => acc + curr, 10 ); // "10" initial value
 //The comma is separating the 1st and 2nd parameters to reduce(): the callback function and the initial value
 console.log(sum);
 
@@ -17,6 +17,8 @@ let sum3 = nums.reduce((acc, curr) => acc + curr);
 //The comma is separating the 1st and 2nd parameters to reduce(): the callback function and the initial value
 console.log("curr or current value starts at the second value if it's not told what number to start on. in this case, current value is 1", sum3);
 console.log(sum3);
+
+// always be explicit with your first value as it could cause issues with your code if you do not. In these cases 10 or 0 are the first values
 
 let sum1 = nums.reduce((acc, curr) => {
   console.log(
@@ -48,10 +50,30 @@ const teamMembers = [
     name: 'Kelly',
     profession: 'Designer',
     yrsExperience: 3
+  },
+  {
+    name: 'Tom',
+    profession: 'manager',
+    yrsExperience: 10
   }
 ];
 
 // Totaling a specific object property
 
+let totalExperience = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience, 0);
+console.log(totalExperience);
 
 // Grouping by a property, and totaling it too
+
+// {Developer: 12, Designer: 4} This the object that we want
+let experienceProfession = teamMembers.reduce((acc, curr) => {
+  let key = curr.profession;
+  if (!acc[key]) {
+    acc[key] = curr.yrsExperience;
+  } else {
+    acc[key] += curr.yrsExperience;
+  }
+  return acc;
+}, {});
+
+console.log(experienceProfession);
